5b40ff76e645f19d356b2c1fe6ede749b451634b
stephenA-ANTHONIUS <Stephen.anthonius12345@gmail.com>
2026-05-21 09:04:40 +0700

    Update OfflineTransaction.php

--- app/Models/OfflineTransaction.php
+++ app/Models/OfflineTransaction.php
@@ -20,15 +20,12 @@ class OfflineTransaction extends Model
 
     protected static function booted()
     {
-        // Event ini dipicu tepat sebelum data OfflineTransaction dihapus
         static::deleting(function ($transaction) {
             DB::transaction(function () use ($transaction) {
-                // 1. Ambil semua item terkait dan kembalikan stoknya ke tabel produk
                 foreach ($transaction->items as $item) {
                     $product = Product::query()->find($item->product_id);
                     
                     if ($product) {
-                        // Tambahkan kembali stok sesuai qty yang dihapus
                         DB::table('products')
                             ->where('id', $product->id)
                             ->increment('jumlah_stok', $item->qty);
