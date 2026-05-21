ef701bab99e995ae11217eedadb854cd2cad6bbd
stephenA-ANTHONIUS <Stephen.anthonius12345@gmail.com>
2026-05-21 11:27:21 +0700

    debug: expose real error

--- api/index.php
+++ api/index.php
@@ -1,5 +1,17 @@
 <?php
 
+// Tangkap error PHP sebelum Laravel handle
+set_exception_handler(function($e) {
+    http_response_code(500);
+    echo '<pre style="color:red;font-size:14px;">';
+    echo '<b>REAL ERROR:</b> ' . get_class($e) . "\n";
+    echo '<b>Message:</b> ' . $e->getMessage() . "\n";
+    echo '<b>File:</b> ' . $e->getFile() . ':' . $e->getLine() . "\n";
+    echo '<b>Trace:</b>' . "\n" . $e->getTraceAsString();
+    echo '</pre>';
+    exit(1);
+});
+
 $tmpDirectories = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache/data',
@@ -13,7 +25,6 @@ foreach ($tmpDirectories as $dir) {
     }
 }
 
-// Gunakan putenv() agar Laravel pasti baca nilai ini
 putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
 putenv('SESSION_DRIVER=cookie');
 putenv('CACHE_STORE=array');
@@ -22,6 +33,5 @@ putenv('LOG_CHANNEL=stderr');
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 $_ENV['CACHE_STORE'] = 'array';
-$_ENV['LOG_CHANNEL'] = 'stderr';
 
 require __DIR__ . '/../public/index.php';
