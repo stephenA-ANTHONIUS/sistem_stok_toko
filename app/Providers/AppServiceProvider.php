f9028594ba6f53de7db40d9a694bc1687d61c145
stephenA-ANTHONIUS <Stephen.anthonius12345@gmail.com>
2026-05-21 10:07:03 +0700

    Fix Laravel 11 View engine for Vercel deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,12 +11,21 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        if (!str_contains(request()->getBaseUrl(), 'localhost') && !$this->app->bound('view')) {
+            $this->app->register(\Illuminate\View\ViewServiceProvider::class);
+        }
+    }
 
     public function boot(): void
     {
         Paginator::useTailwind();
 
         OfflineTransactionItem::observe(OfflineTransactionItemObserver::class);
+        
+        if (config('app.env') !== 'local') {
+            config(['view.compiled' => '/tmp/storage/framework/views']);
+        }
     }
 }
