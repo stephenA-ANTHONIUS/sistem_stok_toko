f12c060c2d2c73b9e9256597cfd48bfa470383c6
stephenA-ANTHONIUS <Stephen.anthonius12345@gmail.com>
2026-05-21 10:30:20 +0700

    k

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,13 +1,26 @@
-public function register(): void
-{
-    // kosongkan, tidak perlu isi apapun
-}
+<?php
 
-public function boot(): void
+namespace App\Providers;
+
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
+use App\Models\OfflineTransactionItem;
+use App\Models\StockEntryItem;
+use App\Observers\OfflineTransactionItemObserver;
+use App\Observers\StockEntryItemObserver;
+
+class AppServiceProvider extends ServiceProvider
 {
-    Paginator::useTailwind();
-    OfflineTransactionItem::observe(OfflineTransactionItemObserver::class);
-    StockEntryItem::observe(StockEntryItemObserver::class);
-    
-    config(['view.compiled' => '/tmp/storage/framework/views']);
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        Paginator::useTailwind();
+        OfflineTransactionItem::observe(OfflineTransactionItemObserver::class);
+        StockEntryItem::observe(StockEntryItem::class);
+        config(['view.compiled' => '/tmp/storage/framework/views']);
+    }
 }
